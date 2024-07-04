ee8e7b908e602adaf58c576c0b71a5c7f0775670
rs545837 <114828377+rs545837@users.noreply.github.com>
2024-07-05 01:10:59 +0530

    Update phonemized.py

--- makeDataset/tools/phonemized.py
+++ makeDataset/tools/phonemized.py
@@ -2,9 +2,7 @@ import argparse
 from phonemizer import phonemize
 import os
 from tqdm import tqdm
-# from phonemizer.backend.espeak.wrapper import EspeakWrapper                ## For Windows
-# EspeakWrapper.set_library("C:\Program Files\eSpeak NG\libespeak-ng.dll")  ## For Windows
-
+import re
 
 # argument parser
 parser = argparse.ArgumentParser(description="Phonemize transcriptions.")
@@ -26,15 +24,14 @@ filenames = []
 transcriptions = []
 speakers = []
 phonemized_lines = []
-for (
-    line
-) in lines:  # Split filenames, text and speaker without phonemizing. Prevents mem error
+
+for line in lines:  # Split filenames, text and speaker without phonemizing. Prevents mem error
     filename, transcription, speaker = line.strip().split("|")
     filenames.append(filename)
     transcriptions.append(transcription)
     speakers.append(speaker)
 
-# Phonemize all text in one go to avoid triggering  mem error
+# Phonemize all text in one go to avoid triggering mem error
 phonemized = phonemize(
     transcriptions,
     language=args.language,
@@ -48,21 +45,27 @@ for i in tqdm(range(len(filenames))):
         (filenames[i], f"{filenames[i]}|{phonemized[i]}|{speakers[i]}\n")
     )
 
+def extract_number(filename):
+    # Try to extract a number from the filename
+    match = re.search(r'\d+', filename)
+    if match:
+        return int(match.group())
+    else:
+        # If no number is found, return the filename as a string
+        # This will sort non-numeric filenames alphabetically after numeric ones
+        return filename
 
-phonemized_lines.sort(key=lambda x: int(x[0].split("_")[1].split(".")[0]))
+# Sort the lines using the new extract_number function
+phonemized_lines.sort(key=lambda x: extract_number(x[0]))
 
 # Split training/validation set
 train_lines = phonemized_lines[: int(len(phonemized_lines) * 0.9)]
 val_lines = phonemized_lines[int(len(phonemized_lines) * 0.9) :]
 
-with open(
-    "./trainingdata/train_list.txt", "w+", encoding="utf-8"
-) as f:  # Path for train_list.txt in the training data folder
+with open("./trainingdata/train_list.txt", "w+", encoding="utf-8") as f:  # Path for train_list.txt in the training data folder
     for _, line in train_lines:
         f.write(line)
 
-with open(
-    "./trainingdata/val_list.txt", "w+", encoding="utf-8"
-) as f:  # Path for val_list.txt in the training data folder
+with open("./trainingdata/val_list.txt", "w+", encoding="utf-8") as f:  # Path for val_list.txt in the training data folder
     for _, line in val_lines:
         f.write(line)
